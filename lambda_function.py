edbee169d5eff902c95bb9bc0d0ab801c03656da
luist@psma.com.au <qq97qf24ffL>
2022-10-10 03:25:20 +1100

    git ignore

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import pandas as pd
 def lambda_handler(event, context):
     d = {'col1': [1,2], 'col2': [3,4]}
     df = pd.Dataframe(data=d)
-    pribnt(df)
+    print(df)
     print('----- Done -----')
 
     
